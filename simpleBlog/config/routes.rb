40d556ea568741807ae2fe59084de3d25dcf1f92
Hien Le <lhien11@gmail.com>
2016-11-14 18:20:31 -0600

    add form-control

--- simpleBlog/config/routes.rb
+++ simpleBlog/config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'posts#index'
+   root 'posts#index', as: 'home'
 
-   get 'about' => 'pages#about'
+   get 'about' => 'pages#about', as: 'about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
