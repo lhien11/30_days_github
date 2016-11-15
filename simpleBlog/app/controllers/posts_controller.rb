405f88eca4cd3fc620bb86db45b596fa24f3f006
Hien Le <lhien11@gmail.com>
2016-11-14 19:02:36 -0600

    Add Delete functionality

--- simpleBlog/app/controllers/posts_controller.rb
+++ simpleBlog/app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
 
   end
 
@@ -14,8 +15,33 @@ class PostsController < ApplicationController
     #render plain: params[:post].inspect
     @post = Post.new(post_params)
 
-    @post.save
-    redirect_to @post
+    if(@post.save)
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if(@post.update(post_params))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
   end
 
   private def post_params
